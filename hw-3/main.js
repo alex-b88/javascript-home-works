9b6924784cfe058c66d40f4385140b9e170bbdc9
alex <alex88.b@icloud.com>
2024-08-10 01:34:58 +0300

    hw3 done

--- hw-3/main.js
+++ hw-3/main.js
@@ -137,21 +137,76 @@
 //     {name: 'max', age: 31, status: true}
 // ];
 // за допомоги циклу вивести:
-//     - користувачів зі статусом true
+// - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
 
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+    let users = [
+        {name: 'vasya', age: 31, status: false},
+        {name: 'petya', age: 30, status: true},
+        {name: 'kolya', age: 29, status: true},
+        {name: 'olya', age: 28, status: false},
+        {name: 'max', age: 30, status: true},
+        {name: 'anya', age: 31, status: false},
+        {name: 'oleg', age: 28, status: false},
+        {name: 'andrey', age: 29, status: true},
+        {name: 'masha', age: 30, status: true},
+        {name: 'olya', age: 31, status: false},
+        {name: 'max', age: 31, status: true}
+    ];
 
+    document.write(`<div class="lists">`);
+            document.write(`<table>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Status</th>
+                </tr>
+            `);
+            for (const user of users) {
+                if(user.status){
+                    document.write(`<tr>
+                    <td>${user.name}</td>
+                    <td>${user.age}</td>
+                    <td>${user.status}</td>
+                </tr>`)
+                }
+            }
+            document.write(`</table>`);
+
+            document.write(`<table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Status</th>
+                    </tr>
+                `);
+            for (const user of users) {
+                if(!user.status){
+                    document.write(`<tr>
+                        <td>${user.name}</td>
+                        <td>${user.age}</td>
+                        <td>${user.status}</td>
+                    </tr>`)
+                }
+            }
+            document.write(`</table>`);
+
+            document.write(`<table>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Age</th>
+                                    <th>Status</th>
+                                </tr>
+                            `);
+            for (const user of users) {
+                if(user.age>30){
+                    document.write(`<tr>
+                                    <td>${user.name}</td>
+                                    <td>${user.age}</td>
+                                    <td>${user.status}</td>
+                                </tr>`)
+                }
+            }
+            document.write(`</table>`);
+    document.write(`</div>`)
